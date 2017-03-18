412e0df379ee5d86244850d6d9fba944724e4ec0
Adam Jahur <adamnjahur@gmail.com>
2017-03-18 13:45:27 -0400

    fixing stuff and added stripe

--- Gateway/resources/views/homeCustomer.blade.php
+++ Gateway/resources/views/homeCustomer.blade.php
@@ -43,7 +43,7 @@
 		</div><!-- /.container-fluid -->
 	</nav>
 <div class="container">
-	<div class="row" style="border: 1px black solid">
+	<div class="row">
 		<div class="col-md-4">
 			<h4 id="company">Best Buy</h4>
 			<h4 id="address">11 Orlando Way, Orlando FL, 32828</h4>
@@ -61,76 +61,9 @@
 	</div>
 </div>
 
-	<div class="row" style=" margin-top: 50px;">
-		<div class="col-md-7" style="border: solid 2px black;">
-			 <form class="form-horizontal" role="form">
-    <fieldset style="margin-right: 300px">
-      <legend>Payment</legend>
-      <div class="form-group">
-        <label class="col-sm-3 control-label" for="card-holder-name">Name on Card</label>
-        <div class="col-sm-9">
-          <input type="text" class="form-control" name="card-holder-name" id="card-holder-name" placeholder="Card Holder's Name">
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-3 control-label" for="card-number">Card Number</label>
-        <div class="col-sm-9">
-          <input type="text" class="form-control" name="card-number" id="card-number" placeholder="Debit/Credit Card Number">
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-3 control-label" for="expiry-month">Expiration Date</label>
-        <div class="col-sm-9">
-          <div class="row">
-            <div class="col-xs-3">
-              <select class="form-control col-sm-2" name="expiry-month" id="expiry-month">
-                <option>Month</option>
-                <option value="01">Jan (01)</option>
-                <option value="02">Feb (02)</option>
-                <option value="03">Mar (03)</option>
-                <option value="04">Apr (04)</option>
-                <option value="05">May (05)</option>
-                <option value="06">June (06)</option>
-                <option value="07">July (07)</option>
-                <option value="08">Aug (08)</option>
-                <option value="09">Sep (09)</option>
-                <option value="10">Oct (10)</option>
-                <option value="11">Nov (11)</option>
-                <option value="12">Dec (12)</option>
-              </select>
-            </div>
-            <div class="col-xs-3">
-              <select class="form-control" name="expiry-year">
-                <option value="13">2013</option>
-                <option value="14">2014</option>
-                <option value="15">2015</option>
-                <option value="16">2016</option>
-                <option value="17">2017</option>
-                <option value="18">2018</option>
-                <option value="19">2019</option>
-                <option value="20">2020</option>
-                <option value="21">2021</option>
-                <option value="22">2022</option>
-                <option value="23">2023</option>
-              </select>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-3 control-label" for="cvv">Card CVV</label>
-        <div class="col-sm-3">
-          <input type="text" class="form-control" name="cvv" id="cvv" placeholder="Security Code">
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="col-sm-offset-3 col-sm-9">
-          <button type="button" class="btn btn-success">Pay Now</button>
-        </div>
-      </div>
-    </fieldset>
-  </form>
-		</div>
+    <br>
+
+
 
 	<div class="col-md-5" style="border: solid 2px black;">
 		<h3 style="text-align: center;">Table of Services</h3><br>
@@ -138,8 +71,23 @@
 		<h5 id="dueDate">Due Date: 03/17/17</h5><br><br>
 		<h5 id="posted">Date Posted: 02/17/17</h5><br>
 
+        <form action=" method="POST">
+        <script
+                src="https://checkout.stripe.com/v2/checkout.js" class="stripe-button"
+                data-key="pk_test_G5YhIkq2PEq84lwU064TZENT"
+                data-amount="2000"
+                data-name="Demo Site"
+                data-description="2 widgets ($20.00)"
+                data-image="/128x128.png">
+        </script>
+        </form>
+        <br>
+
 	</div>
 	</div>
+
+
+
 
 
 </body>
